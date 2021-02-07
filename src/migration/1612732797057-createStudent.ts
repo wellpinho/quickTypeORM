93bb004178924360b62f4adef05d6f65a3077f3c
wellpinho <wellpinho@outlook.com>
2021-02-07 18:23:38 -0300

    Criado migration Student

---
+++ src/migration/1612732797057-createStudent.ts
@@ -0,0 +1,43 @@
+import { query } from "express";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createStudent1612732797057 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'student',
+      columns: [
+        {
+          name: 'id',
+          type: 'uuid',
+          isPrimary: true,
+          generationStrategy: 'uuid',
+          default: 'uuid_generate_v4()'
+        },
+        {
+          name: 'name',
+          type: 'varchar'
+        },
+        {
+          name: 'key',
+          type: 'varchar'
+        },
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          default: 'now()'
+        },
+        {
+          name: 'updated_at',
+          type: 'timestamp',
+          default: 'now()'
+        }
+      ]
+    }))
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('student')
+  }
+
+}
